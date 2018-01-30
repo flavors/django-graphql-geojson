c21f798d0fefead09818af7157c2a16e4db55ad6
mongkok <domake.io@gmail.com>
2018-01-31 02:06:13 +0530

    Geometry converter

--- graphql_geojson/converter.py
+++ graphql_geojson/converter.py
@@ -7,23 +7,23 @@ from graphene.types.generic import GenericScalar
 from graphene_django.converter import convert_django_field
 
 
-def geojson_resolver(attname, default_value, root, info, **args):
+def geometry_resolver(attname, default_value, root, info, **args):
     if default_value is not None:
         root = root or default_value
     return json.loads(root.geojson)[attname]
 
 
-class GeoJSON(graphene.ObjectType):
+class Geometry(graphene.ObjectType):
     type = graphene.String()
     coordinates = GenericScalar()
 
     class Meta:
-        default_resolver = geojson_resolver
+        default_resolver = geometry_resolver
 
 
 @convert_django_field.register(GeometryField)
-def convert_field_to_geojson(field, registry=None):
+def convert_field_to_geometry(field, registry=None):
     return graphene.Field(
-        GeoJSON,
+        Geometry,
         description=field.help_text,
         required=not field.null)
