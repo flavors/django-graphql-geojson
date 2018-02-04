27ebd4f58da0004e5cedf00c48314981acd971f8
mongkok <domake.io@gmail.com>
2018-02-04 20:25:06 +0530

    Typos

--- tests/relay/filters.py
+++ tests/relay/filters.py
@@ -1,12 +1,9 @@
-import django_filters
-
 from graphql_geojson.filters import GeometryFilterSet
 
 from .. import models
 
 
 class PlaceFilter(GeometryFilterSet):
-    location__test = django_filters.BooleanFilter()
 
     class Meta:
         model = models.Place
