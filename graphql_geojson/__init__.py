d519dd144577aeefc4e5a7cdd08b0794fa4eefef
mongkok <domake.io@gmail.com>
2018-02-04 19:20:04 +0530

    Import types

--- graphql_geojson/__init__.py
+++ graphql_geojson/__init__.py
@@ -1,5 +1,10 @@
-from . import converter, types
+from . import converter
+from .types import Geometry, GeoJSONType
 
-__all__ = ['converter', 'types']
+__all__ = [
+    'converter',
+    'Geometry',
+    'GeoJSONType',
+]
 
-__version__ = '0.0.2'
+__version__ = '0.0.3.dev0'
