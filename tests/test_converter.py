b83258fa46958b9c4faf7b18c79b9fb357fc2642
mongkok <domake.io@gmail.com>
2018-02-13 00:21:55 +0530

    Added test resolver

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -1,33 +1,12 @@
-from django.contrib.gis import geos
 from django.contrib.gis.db import models
 from django.test import TestCase
 
 from graphene_django.converter import convert_django_field
 
-from graphql_geojson import resolver, types
+from graphql_geojson import types
 
 
 class ConverterTests(TestCase):
-
-    def test_geometry_resolver(self):
-        geometry = geos.Point(0, 1)
-        resolved = resolver.geometry_resolver(
-            attname='type',
-            default_value=None,
-            root=geometry,
-            info=None)
-
-        self.assertEqual(resolved, 'Point')
-
-    def test_geometry_default_resolver(self):
-        geometry = geos.LineString((0, 0), (0, 1))
-        resolved = resolver.geometry_resolver(
-            attname='type',
-            default_value=geometry,
-            root=None,
-            info=None)
-
-        self.assertEqual(resolved, 'LineString')
 
     def test_convert_geometry(self):
         field = models.PointField()
---
+++ tests/test_resolver.py
@@ -0,0 +1,40 @@
+from unittest.mock import MagicMock
+
+from django.contrib.gis import geos
+from django.test import TestCase
+
+from graphql_geojson import resolver
+
+from .models import Place
+
+
+class ResolverTests(TestCase):
+
+    def test_geometry_resolver(self):
+        geometry = geos.Point(0, 1)
+        resolved = resolver.geometry_resolver(
+            attname='type',
+            default_value=None,
+            root=geometry,
+            info=None)
+
+        self.assertEqual(resolved, 'Point')
+
+    def test_geometry_default_resolver(self):
+        geometry = geos.LineString((0, 0), (0, 1))
+        resolved = resolver.geometry_resolver(
+            attname='type',
+            default_value=geometry,
+            root=None,
+            info=None)
+
+        self.assertEqual(resolved, 'LineString')
+
+    def test_feature_bbox_resolver_null_geometry(self):
+        resolved = resolver.feature_resolver(
+            attname='bbox',
+            default_value='location',
+            root=Place(),
+            info=MagicMock())
+
+        self.assertIsNone(resolved)
